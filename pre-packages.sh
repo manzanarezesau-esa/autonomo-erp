00166b37bd482af1a8e3eceeaa75405554ed0dae
manzanarezesau-esa <manzanarezesau@gmail.com>
2026-09-08 14:40:23 +0200

    Delete pre-packages.sh

--- pre-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Use an `if` to prevent re-running unnecessarily (though it's idempotent)
-if [ ! -f /etc/apt/apt.conf.d/99-expire ]; then
-  echo "Aplicando parche para repositorios de seguridad Debian Bullseye caducados..."
-  echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99-expire
-else
-  echo "El parche ya está aplicado."
-fi
